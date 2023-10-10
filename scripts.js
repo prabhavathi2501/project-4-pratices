f65c3d791712f2cfde4e4d56783642d370a29225
prabhavathi2501 <prabhashok2501@gmail.com>
2023-10-10 18:33:39 +0530

    upadate

--- scripts.js
+++ scripts.js
@@ -1,33 +1,4 @@
-/*const res = fetch("https://jsonplaceholder.typicode.com/users");
-res
-  .then((data) => data.json())
-  .then((data1) => {
-    for (var i = 0; i < 16; i++) {
-      //console.log(data1[i])
-      const div = document.createElement("div");
-      div.innerHTML = `<div class="row row-cols-1 row-cols-md-4 ">
-        <div class="col">
-          <div class="card">
-            
-            <div class="card-body">
-              <h5 class="card-title">name:${data1[0].name}</h5>
-              <p class="card-text"><b><i>email: ${data1[0].email}</i></b></p>
-              <p class="card-text"><b><i>street: ${data1[0].address.street}</i></b></p>
-              <p class="card-text"><b><i>city: ${data1[0].address.city}</i></b></p>
-              
-            </div>
-          </div>
-        </div>
-        </div>`;
-      document.body.append(div);
-    }
-  });*/
-
-
-
-
-
-  async function foo(){
+async function foo(){
     try {
      const res = fetch("https://jsonplaceholder.typicode.com/users");
    res
